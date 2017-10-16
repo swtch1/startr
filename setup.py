21b60c5889335c92a6a9673a63974fc24ea4c7da
212563094 <joshua.thornton@ge.com>
2017-10-15 23:54:32 -0400

    Adding setup.py.

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-pyyaml
+from setuptools import setup
+
+setup(
+    name='startr',
+    version='0.1',
+    description='Dependency aware farm role starter for Scalr.',
+    url='https://github.build.ge.com/CoreTechAutomation/startr',
+    author='Joshua Thornton',
+    author_email='joshua.thornton@ge.com',
+    packages='startr',
+    install_requires=[
+        'pyyaml'
+    ]
+)
+
 # TODO: Add all the config files to the right place.
 # TODO: Setup an alias for the tool so it can be used from the command line.
